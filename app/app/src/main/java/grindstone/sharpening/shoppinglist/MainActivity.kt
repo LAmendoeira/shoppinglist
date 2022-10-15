fa74f22a75736a8e5f7002a78929ca3cd20c521f
LAmendoeira <6997664+LAmendoeira@users.noreply.github.com>
2022-10-15 01:06:07 +0100

    Added FAB to add new product; Created dialog to input product name; Update listview with newproduct;

--- app/app/src/main/java/grindstone/sharpening/shoppinglist/MainActivity.kt
+++ app/app/src/main/java/grindstone/sharpening/shoppinglist/MainActivity.kt
@@ -1,30 +1,45 @@
 package grindstone.sharpening.shoppinglist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import grindstone.sharpening.shoppinglist.adapters.ListViewAdapter
 import grindstone.sharpening.shoppinglist.data.Product
 import grindstone.sharpening.shoppinglist.databinding.ActivityMainBinding
+import grindstone.sharpening.shoppinglist.dialogFragments.AddProductDialogFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AddProductDialogFragment.DialogListener {
 
     private val TAG = "MainActivity"
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var listAdapterProducts: ListViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Initialize buttons event listners
+        initButtons()
+
         //Just a testing list with products
-        val productsList: List<String> = listOf("Arroz", "Bananas", "Massa", "Diogo", "Hamburguer", "Arroz", "Bananas", "Massa", "Diogo", "Hamburguer","Arroz", "Bananas", "Massa", "Diogo", "Hamburguer","Arroz", "Bananas", "Massa", "Diogo", "Hamburguer","Arroz", "Bananas", "Massa", "Diogo", "Hamburguer","Arroz", "Bananas", "Massa", "Diogo", "Hamburguer","Arroz", "Bananas", "Massa", "Diogo", "Hamburguer","Arroz", "Bananas", "Massa", "Diogo", "Hamburguer")
+        val productsList: List<String> = listOf(
+            "Arroz",
+            "Bananas",
+            "Massa",
+            "Diogo",
+            "Hamburguer",
+            "Arroz",
+            "Bananas",
+            "Massa",
+            "Diogo"
+        )
 
         setupListView(initProducts(productsList.sorted()))
-
     }
 
     /**
@@ -33,7 +48,7 @@ class MainActivity : AppCompatActivity() {
      */
     private fun setupListView(productsList: ArrayList<Product>) {
 
-        val listAdapterProducts = ListViewAdapter(this, productsList)
+        listAdapterProducts = ListViewAdapter(this, productsList)
 
         binding.listViewProductsList.adapter = listAdapterProducts
 
@@ -44,12 +59,9 @@ class MainActivity : AppCompatActivity() {
             val linearLayout = view as LinearLayout
 
             val txtView = linearLayout.getChildAt(0) as TextView
-//            txtView.setTextAppearance(R.style.doneText)
-
-//            Toast.makeText(this, "Click on item at $itemAtPos its item id $itemIdAtPos", Toast.LENGTH_SHORT).show()
 
             if (itemAtPos is Product) {
-                if(!itemAtPos.done) {
+                if (!itemAtPos.done) {
                     itemAtPos.done = true
                     txtView.setTextAppearance(R.style.doneText)
                 } else {
@@ -73,6 +85,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         return products
+    }
 
+    /**
+     * Setup event listeners in the buttons for this activity
+     */
+    private fun initButtons() {
+        //FAB add product
+        binding.fabAddProduct.setOnClickListener { view ->
+            val newFragment = AddProductDialogFragment()
+            newFragment.show(supportFragmentManager, "addProduct")
+        }
+    }
+
+    /**
+     * Receives the positive event from the Add Product Dialog
+     */
+    override fun onDialogPositiveClick(dialog: DialogFragment) {
+        val dialogView = dialog.dialog
+        if (dialogView != null) {
+            val editTextProduct: EditText = dialogView.findViewById(R.id.editTextAddProduct)
+            addProduct(editTextProduct.text.toString())
+        }
+    }
+
+    /**
+     * Receives the negative event from the Add Product Dialog
+     */
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        dialog.dialog?.cancel()
+    }
+
+    /**
+     * Adds a new product to the ListView
+     */
+    private fun addProduct(name: String) {
+        listAdapterProducts.add(Product(name, "", false))
     }
 }
